d1d3411baaa22613562411dfd96f6a51511d286b
Giuls1974 <109532635+Giuls1974@users.noreply.github.com>
2022-07-19 13:58:56 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit
 
-streamlit.title("\n my parents heathy dinner")
-
-streamlit.title("Main disces")
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
